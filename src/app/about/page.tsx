1457766b98e9bc906f0adce068682f78c8c1a4d4
Parchello <129171831+Parchello@users.noreply.github.com>
2025-06-20 22:50:47 +0300

    add about page (#2)
    * prettier format
    
    * add about page with some styles
    
    * fix
    
    * padding change

---
+++ src/app/about/page.tsx
@@ -0,0 +1,35 @@
+import Typography from '@/components/general/Typography/Typography';
+import React from 'react';
+import styles from './page.module.scss';
+
+const About = () => {
+  return (
+    <div className={styles.aboutSection}>
+      <Typography variant="h1" className={styles.heading}>
+        About
+      </Typography>
+      <Typography>
+        My Journey in Photography I started photography in August of 2024. I am
+        currently based in Austria, and I am fortunate enough be surrounded by
+        plenty of nature and wildlife. Additionally, I am close to beautiful
+        nature reserves where I can explore and capture the wonders of the
+        natural world. My initial inspiration came from a photographer named
+        Robert Marc Lehmann. A few months later, I discovered my mom s
+        twenty-year-old camera lying around. It was that moment when I began
+        taking pictures. I quickly developed a passion for photography, leading
+        me to purchase my first camera just a few weeks later. Now, almost a
+        year into my photography journey, I have dedicatet a significant amount
+        of time capturing the wildlife of Austria. Photography provides me a
+        sense of freedom and allows me to be present in the moment. It is a
+        powerful outlet for my creativity, as I strive to capture unique
+        pictures and moments. I thoroughly enjoy learning new techniques and
+        embracing the present. While I acknowledge that I am still a relatively
+        new photographer, I am very pleased with my progress this far. I m
+        looking forward to continuing my photography journey and seeing what the
+        future holds.
+      </Typography>
+    </div>
+  );
+};
+
+export default About;
