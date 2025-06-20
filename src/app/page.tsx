bd6e7858e7e3d2622651c3d8e7ebe1c30e8225e3
Parchello <parchellos@gmail.com>
2025-06-20 14:23:34 +0300

    update main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import About from '@/components/About/About';
 import Typography from '@/components/general/Typography/Typography';
 
 export default function Home() {
-  return <Typography variant="h1">Hello world</Typography>;
+  return (
+    <>
+      <About />
+      <Typography variant="h1">Hello world</Typography>
+    </>
+  );
 }
