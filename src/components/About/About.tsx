afc973cdcd3bed67918c68eba5a143f815cf768b
Parchello <parchellos@gmail.com>
2025-06-20 14:17:32 +0300

    about comp

---
+++ src/components/About/About.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const About: React.FC = () => {
+  return (
+    <section>
+      <h1>About</h1>
+      <p>
+        I started photography in August of 2024. I am currently based in
+        Austria, and I am fortunate enough be surrounded by plenty of nature and
+        wildlife. Additionally, I am close to beautiful nature reserves where I
+        can explore and capture the wonders of the natural world. My initial
+        inspiration came from a photographer named Robert Marc Lehmann. A few
+        months later, I discovered my mom s twenty-year-old camera lying around.
+        It was that moment when I began taking pictures. I quickly developed a
+        passion for photography, leading me to purchase my first camera just a
+        few weeks later. Now, almost a year into my photography journey, I have
+        dedicatet a significant amount of time capturing the wildlife of
+        Austria. Photography provides me a sense of freedom and allows me to be
+        present in the moment. It is a powerful outlet for my creativity, as I
+        strive to capture unique pictures and moments. I thoroughly enjoy
+        learning new techniques and embracing the present. While I acknowledge
+        that I am still a relatively new photographer, I am very pleased with my
+        progress this far. I m looking forward to continuing my photography
+        journey and seeing what the future holds.
+      </p>
+    </section>
+  );
+};
+
+export default About;
