51457453170e25115c7ba4edc5d4366cb510ac04
Parchello <parchellos@gmail.com>
2025-06-20 22:24:54 +0300

    add about page with some styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
-import About from '@/components/About/About';
 import Typography from '@/components/general/Typography/Typography';
 
 export default function Home() {
-  return (
-    <>
-      <About />
-      <Typography variant="h1">Hello world</Typography>
-    </>
-  );
+  return <Typography variant="h1">Hello world</Typography>;
 }
