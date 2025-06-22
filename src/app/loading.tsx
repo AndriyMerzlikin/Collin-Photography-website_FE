fd974b465a46acf7738f06915ba874a1893f879a
Parchello <parchellos@gmail.com>
2025-06-22 20:02:23 +0300

    add loader

---
+++ src/app/loading.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles from './loading.module.scss';
+
+const Loading = () => {
+  return (
+    <div className={styles.ldsSpinner}>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+export default Loading;
