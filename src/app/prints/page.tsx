dc72e1ea57ec2de05ec88d60cf16b700d7aacb2d
AndriyMerzlikin <lastheroofme@gmail.com>
2025-06-26 07:55:32 +0300

    added prints

---
+++ src/app/prints/page.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import styles from './page.module.scss';
+import Typography from '@/components/general/Typography/Typography';
+import Image from 'next/image';
+import Link from 'next/link';
+import { ROUTES } from '@/constants/routes';
+
+const PrintsPage = () => {
+  return (
+    <div className={styles.container}>
+      <Typography variant="h2">prints</Typography>
+      <div className={styles.cardBox}>
+        <Link href={ROUTES.DIGITAL} className={styles.cardWrapper}>
+          <div className={styles.imageWrapper}>
+            <Image
+              src="/gallery2.jpg"
+              alt="Digital prints"
+              width={600}
+              height={400}
+              priority
+              className={styles.image}
+            />
+            <Typography variant="h3" className={styles.overlayText}>
+              digital prints
+            </Typography>
+          </div>
+        </Link>
+        <Link href={ROUTES.FRAME} className={styles.cardWrapper}>
+          <div className={styles.imageWrapper}>
+            <Image
+              src="/gallery3.jpg"
+              alt="Frame prints"
+              width={600}
+              height={400}
+              className={styles.image}
+            />
+            <Typography variant="h3" className={styles.overlayText}>
+              frame prints
+            </Typography>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default PrintsPage;
