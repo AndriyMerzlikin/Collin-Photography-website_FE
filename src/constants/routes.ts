a1639a11f0966fca841ba7d50a64342055dd84bf
AndriyMerzlikin <Lastheroofme@gmail.com>
2025-06-26 07:56:56 +0300

    added prints (#12)

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,6 +1,8 @@
 export const ROUTES = {
   HOME: '/',
   PRINTS: '/prints',
+  DIGITAL: '/prints/digital',
+  FRAME: '/prints/frame',
   PORTFOLIO: '/portfolio',
   ABOUT: '/about',
   CONTACTS: '/contacts',
