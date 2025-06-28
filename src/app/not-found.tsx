462c05af1f3406abbddab6d3886e3a98d3f85a5d
Parchello <parchellos@gmail.com>
2025-06-28 15:30:34 +0300

    removed typography from 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,21 +1,17 @@
 import React from 'react';
 import styles from './not-found.module.scss';
-import Typography from '@/components/general/Typography/Typography';
+
 import Link from 'next/link';
 import { ROUTES } from '@/constants/routes';
 
 const NotFoundPage = () => (
   <div className={styles.notFoundContainer}>
-    <Typography variant="h1" className={styles.notFoundTitle}>
-      404 - Page Not Found
-    </Typography>
-    <Typography className={styles.notFoundMessage}>
+    <h1 className={styles.notFoundTitle}>404 - Page Not Found</h1>
+    <p className={styles.notFoundMessage}>
       Sorry, the page you are looking for does not exist.
-    </Typography>
+    </p>
     <Link href={ROUTES.HOME} className={styles.homeLink}>
-      <Typography variant="body-large" className={styles.homeLinkText}>
-        Go to Home page
-      </Typography>
+      <p className={styles.homeLinkText}>Go to Home page</p>
     </Link>
   </div>
 );
