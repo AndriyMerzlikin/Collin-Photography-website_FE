131e8c6933780adcbcc1dc7a08c56377574f4f0d
Parchello <129171831+Parchello@users.noreply.github.com>
2025-06-28 22:32:38 +0300

    fix about styles page (#13)
    * fix about styles page
    
    * removed typography from 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import styles from './not-found.module.scss';
-import Typography from '@/components/general/Typography/Typography';
+
 import Link from 'next/link';
 import { ROUTES } from '@/constants/routes';
 
 const NotFoundPage = () => (
   <div className={styles.notFoundContainer}>
-    <Typography variant="h1">404 - Page Not Found</Typography>
-    <Typography>Sorry, the page you are looking for does not exist.</Typography>
+    <h1 className={styles.notFoundTitle}>404 - Page Not Found</h1>
+    <p className={styles.notFoundMessage}>
+      Sorry, the page you are looking for does not exist.
+    </p>
     <Link href={ROUTES.HOME} className={styles.homeLink}>
-      <Typography variant="body-large">Go to Home page</Typography>
+      <p className={styles.homeLinkText}>Go to Home page</p>
     </Link>
   </div>
 );
