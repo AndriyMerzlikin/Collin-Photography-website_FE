2a3f991f2e609e297e1eed9b1b1593fac3c34140
AndriyMerzlikin <lastheroofme@gmail.com>
2025-06-30 00:32:55 +0300

    added slider zoom to gallery

--- src/components/GalleryList/GalleryList.tsx
+++ src/components/GalleryList/GalleryList.tsx
@@ -1,7 +1,11 @@
+'use client';
+
 import React from 'react';
 import styles from './GalleryList.module.scss';
 import Image from 'next/image';
 import { FiZoomIn } from 'react-icons/fi';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
+import 'react-photo-view/dist/react-photo-view.css';
 
 const galleryList = [
   'gallery1.jpg',
@@ -18,20 +22,26 @@ const galleryList = [
 
 const GalleryList = () => {
   return (
-    <ul className={styles.container}>
-      {galleryList.map((item, index) => (
-        <li key={index} className={styles.itemBox}>
-          <Image
-            src={`/${item}`}
-            alt={`Gallery image ${index + 1}`}
-            className={styles.image}
-            width={500}
-            height={500}
-          />
-          <FiZoomIn className={styles.iconOverlay} size={50} />
-        </li>
-      ))}
-    </ul>
+    <PhotoProvider>
+      <ul className={styles.container}>
+        {galleryList.map((item, index) => (
+          <li key={index} className={styles.itemBox}>
+            <PhotoView src={`/${item}`}>
+              <div className={styles.imageWrapper}>
+                <Image
+                  src={`/${item}`}
+                  alt={`Gallery image ${index + 1}`}
+                  className={styles.image}
+                  width={500}
+                  height={500}
+                />
+                <FiZoomIn className={styles.iconOverlay} size={50} />
+              </div>
+            </PhotoView>
+          </li>
+        ))}
+      </ul>
+    </PhotoProvider>
   );
 };
 
