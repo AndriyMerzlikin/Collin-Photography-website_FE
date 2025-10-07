c9033c3e1495a0f7a18104b6e8043e1396187903
AndriyMerzlikin <lastheroofme@gmail.com>
2025-10-07 20:02:04 +0300

    fixed setting frameList

--- src/app/prints/frame/page.tsx
+++ src/app/prints/frame/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import styles from './page.module.scss';
 import Typography from '@/components/general/Typography/Typography';
 import Link from 'next/link';
@@ -10,8 +10,11 @@ import { calendarsData } from '@/constants/calendarsData';
 import { CalendarsDataItem } from '@/types/calendarsDataTypes';
 
 const FramePage = () => {
-  const [frameList, setFrameList] =
-    useState<CalendarsDataItem[]>(calendarsData);
+    const [frameList, setFrameList] = useState<CalendarsDataItem[]>([]);
+
+    useEffect(() => {
+        setFrameList(calendarsData);
+    }, []);
 
   return (
     <div className={styles.container}>
