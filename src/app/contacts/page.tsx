896c1fe411eddd0c2e192de9c3eb19e86067497f
AndriyMerzlikin <Lastheroofme@gmail.com>
2026-05-31 23:03:34 +0300

    added socials & new email (#20)

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import styles from './page.module.scss';
 import Typography from '@/components/general/Typography/Typography';
 import { FiInstagram } from 'react-icons/fi';
+import { PiTiktokLogo } from 'react-icons/pi';
+import { FaYoutube } from 'react-icons/fa';
 import Link from 'next/link';
 import ContactForm from '@/components/ContactForm/ContactForm';
+import { SOCIAL_MEDIA } from '@/constants/socialMediaLinks';
 
 const Contacts = () => {
   return (
@@ -13,12 +16,36 @@ const Contacts = () => {
         <Typography variant="body-large">
           We look forward to hear from you!
         </Typography>
-        <Link
-          href="https://www.instagram.com/collinruehrer?igsh=Mnc3aHNnOGc4ZWt4"
-          className={styles.socialButton}
-        >
-          <FiInstagram size={35} />
-        </Link>
+        <Typography variant="body-large">Follow me on Social Media</Typography>
+        <div className={styles.socialsBox}>
+          <Link
+            href={SOCIAL_MEDIA.INSTAGRAM}
+            className={styles.socialButton}
+            title="Instagram"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FiInstagram size={35} />
+          </Link>
+          <Link
+            href={SOCIAL_MEDIA.TIK_TOK}
+            className={styles.socialButton}
+            title="Tiktok"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <PiTiktokLogo size={35} />
+          </Link>
+          <Link
+            href={SOCIAL_MEDIA.YOUTUBE}
+            className={styles.socialButton}
+            title="YouTube"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaYoutube size={35} />
+          </Link>
+        </div>
       </section>
 
       <section className={styles.contactsContainer}>
@@ -33,22 +60,20 @@ const Contacts = () => {
             Contact Collin
           </Typography>
           <Typography variant="body-large" className={styles.infoText}>
-            Interested in working with <span>Collin</span>, the 15-year-old
-            nature and wildlife photographer? Whether you’re an agency looking
-            to showcase fresh talent or you’d like to feature his work in your
-            project, reach out to him by the email.
+            Interested in working with <span>Collin</span>, the 16-year-old
+            wildlife photographer? Whether you’re an agency looking to showcase
+            fresh talent or you’d like to feature his work in your project,
+            reach out to him by the email.
           </Typography>
           <Typography variant="h4" className={styles.infoTitle}>
             email
           </Typography>
 
           <Link
-            href="mailto:collin.r.photography@gmail.com"
+            href={`mailto:${SOCIAL_MEDIA.EMAIL}`}
             className={styles.emailLink}
           >
-            <Typography variant="body-large">
-              collin.r.photography@gmail.com
-            </Typography>
+            <Typography variant="body-medium">{SOCIAL_MEDIA.EMAIL}</Typography>
           </Link>
         </div>
       </section>
---
+++ src/constants/socialMediaLinks.ts
@@ -0,0 +1,6 @@
+export const SOCIAL_MEDIA = {
+  EMAIL: 'collin.photo.business@gmail.com',
+  INSTAGRAM: 'https://www.instagram.com/collin_photo?igsh=Mnc3aHNnOGc4ZWt4',
+  TIK_TOK: 'https://www.tiktok.com/@collin_photography?_r=1&_t=ZN-96chT8KWm1O',
+  YOUTUBE: 'https://www.youtube.com/@Collin-photography',
+};
